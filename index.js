82cf8d1a8de167c3806d8bd7debbb27b2f2ee11e
ibc8 <geveugene@gmail.com>
2024-11-05 20:59:48 +0500

    Первый коммит

---
+++ index.js
@@ -0,0 +1,76 @@
+const sharp = require('sharp');
+let inputText = ("Hello World!") //ввод текста
+inputText += "   ";
+const utf8Encoded = Buffer.from(inputText, 'utf8');
+//! console.log(utf8Encoded);
+
+//& Преобразуем в бинарный вид
+const utf8Array = [];
+for (let i = 0; i < utf8Encoded.length; i++) {
+    const byte = utf8Encoded[i].toString(2).padStart(8, '0');
+    utf8Array.push(...byte.split('').map(Number));
+}
+
+//& console.log("\nДлина:", utf8Array.length);
+const textLength = utf8Array.length;
+
+//& Преобразуем число в бинарную строку
+const binaryString = textLength.toString(2);
+
+//& Дополняем нулями слева до 32 бит (4 байта)
+const paddedBinaryString = binaryString.padStart(21, '0');
+
+//& Преобразуем строку в массив из 0 и 1
+const arrayFromLength = paddedBinaryString.split('').map(Number);
+
+function red(red, array) {
+    red = red - (red % 2);
+    red = red + array;
+    return red;
+}
+
+sharp('input.png')
+    .ensureAlpha() //& Чек альфа канал
+    .raw() //& Получите необработанные данные
+    .toBuffer({ resolveWithObject: true })
+    .then(({ data, info }) => {
+        const { width, height } = info;
+        //! console.log(inputText.length)
+        let count = 0;
+        let count2 = 0;
+        for (let y = 0; y < height; y++) {
+            for (let x = 0; x < width; x++) {
+                const index = (width * y + x) * 4; //& Индекс для RGBA
+                if (count <= 21) { //! контрольная сумма
+                    data[index] = red(data[index], arrayFromLength[count]);
+                    count += 1;
+                    //! console.log(arrayFromLength[count]);
+                    continue;
+                }
+                if ((count <= textLength)) {
+                    data[index] = red(data[index], utf8Array[count2]);
+                    count2 += 1;
+                    count += 1;
+                }
+            }
+        }
+
+        const bytes = [];
+        for (let i = 0; i < utf8Array.length; i += 8) {
+            const byte = utf8Array.slice(i, i + 8).join('');
+            bytes.push(parseInt(byte, 2));
+        }
+
+        //! Создаем буфер из байтов и декодируем в строку
+        const decodedText = Buffer.from(bytes).toString('utf8');
+
+        console.log("Текст:", decodedText);
+        return sharp(data, { raw: { width, height, channels: 4 } })
+            .toFile('output.png');
+    })
+    .then(() => {
+        console.log('Изображение успешно обработано и сохранено!');
+    })
+    .catch(err => {
+        console.error('Ошибка при обработке изображения:', err);
+    });
---
+++ prefix.js
@@ -0,0 +1,69 @@
+const sharp = require('sharp');
+
+
+sharp('output.png')
+    .ensureAlpha() //& Чек альфа канал
+    .raw() //& Необработанные данные
+    .toBuffer({ resolveWithObject: true })
+    .then(({ data, info }) => {
+        const { width, height } = info;
+        let count = 0;
+        let textArray = [];
+        let controlSum = [];
+        let sumFlag = false;
+        let combinedValue = '';
+        let binaryString = 0;
+        let decimalNumber = 0;
+        for (let y = 0; y < height; y++) {
+            for (let x = 0; x < width; x++) {
+                const index = (width * y + x) * 4; //& RGBA
+                if (count <= 21) {
+                    controlSum[count] = data[index] % 2
+
+
+                    binaryString = controlSum.join('');
+
+                    //& Шаг 2: Преобразование бинарной строки в десятичное число
+                    decimalNumber = parseInt(binaryString, 2);
+                    decimalNumber = decimalNumber / 2;
+                    //! console.log(decimalNumber);
+
+                    count += 1
+                    //! console.log(count, "***", data[index].toString(), "***");
+                    continue;
+                }
+
+                if ((count <= decimalNumber)) { // 
+                    //& Получаем значения из RGB каналов
+                    let value1 = data[index].toString() % 2;
+                    textArray.push(value1);
+                    count += 1;
+                    //! console.log("gj", data[index].toString() % 2, count);
+                }
+            }
+        }
+
+        const binaryString2 = textArray.join('');
+
+        //& Разбиваем на 8-битные группы
+        const bytes = [];
+        for (let i = 0; i < binaryString2.length; i += 8) {
+            const byte = binaryString2.slice(i, i + 8);
+            if (byte.length === 8) {
+                bytes.push(parseInt(byte, 2));
+            }
+        }
+
+        //& Преобразуем байты в текст
+        const decodedText = Buffer.from(bytes).toString('utf8');
+
+        console.log("Текст: ", decodedText);
+    })
+
+
+    .then(() => {
+        console.log('Успешно');
+    })
+    .catch(err => {
+        console.error('Ошибка при обработке изображения:', err);
+    });
